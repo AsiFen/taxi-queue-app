34f48e66beb029091c0b042573eefd0b26267cc4
Asisipho <missfente@gmail.com>
2023-05-25 11:53:34 +0200

    test done

--- taxi-queue.js
+++ taxi-queue.js
@@ -1,29 +1,49 @@
 function TaxiQueue() {
-
+	var personsCount = 0;
+	var taxisCount = 0;
 
 	function joinQueue() {
+		personsCount++
+		if (personsCount == 12) {
+			return true
+		}
+
+	}
+
+	function taxiDepart() {
+		console.log(personsCount);
+
+		if(personsCount >= 12 && taxisCount > 0){
+			taxisCount--
+			personsCount = personsCount - 12
+		}
+		console.log(personsCount);
+
 
 	}
 
 	function leaveQueue() {
-
+		if (personsCount > 0) {
+			personsCount--
+		}
 	}
 
 	function joinTaxiQueue() {
-
+		taxisCount++
 	}
 
 	function queueLength() {
 
+		return personsCount
+
+
 	}
 
 	function taxiQueueLength() {
+			return taxisCount
 
 	}
 
-	function taxiDepart(){
-
-	}
 
 	return {
 		joinQueue,
